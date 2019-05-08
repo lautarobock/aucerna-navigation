c3e01731cb4b85edb871d3f5585d58ea2de64a86
Lautaro Cozzani <lautaromail@gmail.com>
2019-05-08 17:19:43 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -114,9 +114,9 @@ class MenuItem {
 
     render() {
         if (this.disabled) {
-            return `<li><a><img src=${this.item.icon}><span>${this.item.name}</span></a></li>`
+            return `<li><a class="disabled"><img src=${this.item.icon}><span>${this.item.name}</span></a></li>`
         } else {
-            return `<li><a class="disabled" href="${this.item.link}"><img src=${this.item.icon}><span>${this.item.name}</span></a></li>`
+            return `<li><a href="${this.item.link}"><img src=${this.item.icon}><span>${this.item.name}</span></a></li>`
         }
     }
 }
